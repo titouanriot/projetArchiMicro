c181d5dddd649c79ad2b4fd17c86175ffc2e2887
Matthieu Cabrera <matthieucabrera64@gmail.com>
2023-04-25 22:54:12 +0200

    fix config back to have a local environment if docker compose not used

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,16 @@
-import os
-
 from pydantic import BaseSettings, Field
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Settings(BaseSettings):
-    db_url: str = Field(..., env='DATABASE_URL')
+    if 'DATABASE_URL' in os.environ:
+        db_url: str = Field(..., env='DATABASE_URL')
+    else :
+        load_dotenv()
+        db_url = "mysql+mysqlconnector://{user}:{password}@127.0.0.1:3306/WatchOurMovieDB".format(
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD'),
+        )
 
 settings = Settings()
